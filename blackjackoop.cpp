68770109fa648d6f3c275739ca961f58febb45b7
cadefarragut <cade_farragut@yahoo.com>
2025-08-18 20:13:11 -0500

    one hand class + few other small adjustments

--- blackjackoop.cpp
+++ blackjackoop.cpp
@@ -5,7 +5,7 @@
 #include <random>
 using namespace std;
 
-vector<int> cards = {1, 1, 1, 1,
+vector<int> cards = {
 		     2, 2, 2, 2,
 		     3, 3, 3, 3,
 	             4, 4, 4, 4,
@@ -20,59 +20,29 @@ vector<int> cards = {1, 1, 1, 1,
 		     13, 13, 13, 13,
 		     14, 14, 14, 14};
 
-class Player {
-	public: 
-		int count;
-		
-		void adjustPlayerCount(int card){
-			if(card > 0 && card < 11){
-				count+=card;
-			}
-			if ( card == 11 || card == 12 || card == 13 ){
-				card += 10;		
-			}
-			if ( card == 14 ){
-
-			}
-	}
-};
-
-class Dealer {
+class Hand {
 	public:
-		int count;
-
-		void adjustDealerCount(int card){
-			if(card > 0 && card < 11){
-				count+=card;
-			}
-			if ( card == 11 || card == 12 || card == 13 ){
-				card += 10;		
-			}
-			if ( card == 14 ){
+		void addCard(int card){
 
 		}
-	}
-};
 
+};
 class Deck{
 	public:
-		Player player;	
-		Dealer dealer;		
 		int deckEmpty(){
 			cout << "Out of Cards. Thanks for Playinig" << endl;
 			return 0;
 		}
 
 		int newCard(){
-			mt19937 rng(time(nullptr));
-			shuffle(cards.begin(), cards.end(), rng);
-			int card = cards.back();
-			if(card){
-				cards.pop_back();
-				return card;
+			
+			if(cards.empty()){
+				deckEmpty();
+				return 0;	
 			}
-			deckEmpty();
-			return 0;	
+			int card = cards.back();	
+			cards.pop_back();
+			return card;	
 		} 
 
 		string cardtoString(int card){
@@ -90,35 +60,35 @@ class Deck{
 			}
 			else return "A";
 		}
-		void deal(){
-			// Player receives 2 cards
-			// Dealer receives 1 card afterwards
-				
-
-			int card = newCard();
-			player.adjustPlayerCount(card);
-			cout << "Dealer: " << cardtoString(card) << endl;	
-			int card2 = newCard();
-			player.adjustPlayerCount(card2);	
-			int card3 = newCard();
-			dealer.adjustDealerCount(card3);
-			cout << "Player: " << cardtoString(card2) << " " << cardtoString(card3) << endl;
-	}
 };
 
 class Game{
 	public:
 		Deck deck;
+		Hand player;
+		Hand dealer;
 
 		void start(){
 			cout << "Welcome to BlackJack" << endl;	
-			deck.deal();
+			deal();
 		}
-		// TODO
-		// End Game if Dealer has BlackJack off rip
-		// Players Turn
-		// Dealers Turn
-		// Ace Config
+
+		void deal(){
+			// Player receives 2 cards
+			// Dealer receives 1 card afterwards
+				
+
+			int card = deck.newCard();
+			player.addCard(card);
+			cout << "Dealer: " << deck.cardtoString(card) << endl;	
+			int card2 = deck.newCard();
+			player.addCard(card2);	
+			int card3 = deck.newCard();
+			dealer.addCard(card3);
+			cout << "Player: " << deck.cardtoString(card2) << " " << deck.cardtoString(card3) << endl;
+	}
+	// TODO End Game if Dealer has BlackJack off rip Players Turn
+		// Dealers Turn Ace Config
 		void playersTurn(){
 
 		}
@@ -130,7 +100,9 @@ class Game{
 
 
 int main(){
-	Game blackjack;
+	Game blackjack;	
+	mt19937 rng(time(nullptr));
+	shuffle(cards.begin(), cards.end(), rng);
 	blackjack.start();
 	blackjack.playersTurn();
 	
